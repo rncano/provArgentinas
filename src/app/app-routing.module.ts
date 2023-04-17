2795eb24e82da7060896ced7b896427cc2af91d5
Emiliano <juanemilianocicirello@gmail.com>
2023-04-17 12:31:26 -0300

    ruteo de aplicacion mas instalacion de bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuariosIngresarComponent } from './components/usuarios-ingresar/usuarios-ingresar.component';
+import { UsuariosListarComponent } from './components/usuarios-listar/usuarios-listar.component';
+import { UsuariosRegistrarComponent } from './components/usuarios-registrar/usuarios-registrar.component';
+import { VistaComponent } from './components/vista/vista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsuariosIngresarComponent
+  },
+  {
+    path: 'usuarios/listar',
+    component: UsuariosListarComponent
+  },
+  {
+    path:'usuarios/registrar',
+    component:UsuariosRegistrarComponent
+  },
+  {
+    path:'usuarios/ingresar',
+    component:UsuariosIngresarComponent
+  },
+  {
+    path:'usuarios/vista',
+    component:VistaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
