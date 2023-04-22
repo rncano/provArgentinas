c539621340354c2a185071355724bfb085ba85c4
Emiliano <juanemilianocicirello@gmail.com>
2023-04-22 20:13:31 -0300

    Arreglo de JSON datos de provincias en el servicio

--- src/app/services/provincia.service.ts
+++ src/app/services/provincia.service.ts
@@ -32,71 +32,53 @@ export class ProvinciaService {
         nombreProvincia: "Santa Fé",
         nombreCapital: "Santa Fé",
         descripcion: "Esta provincia se encuentra en el norte de Argentina, y es la segunda más poblada del país. Cuenta con una amplia variedad de paisajes, desde las playas del río Paraná hasta las llanuras del oeste de la provincia.",
-        imagenProvincia: "./assets/media/cordoba.jpg",
-        videoProvincia: "https://www.youtube.com/embed/FtD7hBacnTo",
+        imagenProvincia: "./assets/media/santafe.jpg",
+        videoProvincia: "https://www.youtube.com/embed/_QppiYQf_6o",
         region:"Centro"
       },
       {
         id: "4",
-        nombreProvincia: "Córdoba",
-        nombreCapital: "Córdoba",
-        descripcion: "Es la Es una provincia ubicada en el centro del país, conocida por sus paisajes serranos y su historia colonial. Es un destino popular para el turismo interno, especialmente para aquellos que buscan aventuras al aire libre.",
-        imagenProvincia: "./assets/media/cordoba.jpg",
-        videoProvincia: "https://www.youtube.com/embed/FtD7hBacnTo",
+        nombreProvincia: "Mendoza",
+        nombreCapital: "Mendoza",
+        descripcion: "Es una provincia ubicada en el oeste de Argentina, conocida por sus viñedos y sus paisajes de montañas. Es un destino popular para el turismo enológico, y también es una base popular para aquellos que desean explorar los Andes.",
+        imagenProvincia: "./assets/media/mendoza.jpg",
+        videoProvincia: "https://www.youtube.com/watch?v=3dzO0avyDhI",
         region:"Centro"
       },
       {
         id: "5",
-        nombreProvincia: "Córdoba ",
-        nombreCapital: "Córdoba",
-        descripcion: "Es la Es una provincia ubicada en el centro del país, conocida por sus paisajes serranos y su historia colonial. Es un destino popular para el turismo interno, especialmente para aquellos que buscan aventuras al aire libre.",
-        imagenProvincia: "./assets/media/cordoba.jpg",
-        videoProvincia: "https://www.youtube.com/embed/FtD7hBacnTo",
+        nombreProvincia: "Tucuman ",
+        nombreCapital: "Tucuman",
+        descripcion: "Esta provincia se encuentra en el noroeste de Argentina, y es conocida por su historia colonial y sus paisajes de montañas. Es el hogar de las ruinas de Quilmes, un importante sitio arqueológico que data de la época precolombina.",
+        imagenProvincia: "./assets/media/tucuman.jpg",
+        videoProvincia: "https://www.youtube.com/watch?v=3dzO0avyDhI",
         region:"Centro"
       },
       {
         id: "6",
-        nombreProvincia: "Córdoba ",
-        nombreCapital: "Córdoba",
-        descripcion: "Es la Es una provincia ubicada en el centro del país, conocida por sus paisajes serranos y su historia colonial. Es un destino popular para el turismo interno, especialmente para aquellos que buscan aventuras al aire libre.",
-        imagenProvincia: "./assets/media/cordoba.jpg",
-        videoProvincia: "https://www.youtube.com/embed/FtD7hBacnTo",
+        nombreProvincia: "Salta ",
+        nombreCapital: "Salta",
+        descripcion: "Esta provincia se encuentra en el noroeste de Argentina, y es conocida por su historia colonial y sus paisajes de montañas. Es el hogar de las ruinas de Quilmes, un importante sitio arqueológico que data de la época precolombina.",
+        imagenProvincia: "./assets/media/salta.jpg",
+        videoProvincia: "https://www.youtube.com/watch?v=W_n6DyZD2oI",
         region:"Centro"
       },
       {
         id: "7",
-        nombreProvincia: "Córdoba ",
-        nombreCapital: "Córdoba",
-        descripcion: "Es la Es una provincia ubicada en el centro del país, conocida por sus paisajes serranos y su historia colonial. Es un destino popular para el turismo interno, especialmente para aquellos que buscan aventuras al aire libre.",
-        imagenProvincia: "./assets/media/cordoba.jpg",
-        videoProvincia: "https://www.youtube.com/embed/FtD7hBacnTo",
+        nombreProvincia: "San Juan ",
+        nombreCapital: "San Juan",
+        descripcion: "Esta provincia se encuentra en el noroeste de Argentina, y es conocida por su historia colonial y sus paisajes de montañas. Es el hogar de las ruinas de Quilmes, un importante sitio arqueológico que data de la época precolombina.",
+        imagenProvincia: "./assets/media/sanjuan.jpg",
+        videoProvincia: "https://www.youtube.com/watch?v=UhXwtmuuiQs",
         region:"Centro"
       },
       {
         id: "8",
-        nombreProvincia: "Córdoba ",
-        nombreCapital: "Córdoba",
-        descripcion: "Es la Es una provincia ubicada en el centro del país, conocida por sus paisajes serranos y su historia colonial. Es un destino popular para el turismo interno, especialmente para aquellos que buscan aventuras al aire libre.",
-        imagenProvincia: "./assets/media/cordoba.jpg",
-        videoProvincia: "https://www.youtube.com/embed/FtD7hBacnTo",
-        region:"Centro"
-      },
-      {
-        id: "9",
-        nombreProvincia: "Córdoba ",
-        nombreCapital: "Córdoba",
-        descripcion: "Es la Es una provincia ubicada en el centro del país, conocida por sus paisajes serranos y su historia colonial. Es un destino popular para el turismo interno, especialmente para aquellos que buscan aventuras al aire libre.",
-        imagenProvincia: "./assets/media/cordoba.jpg",
-        videoProvincia: "https://www.youtube.com/embed/FtD7hBacnTo",
-        region:"Centro"
-      },
-      {
-        id: "10",
-        nombreProvincia: "Córdoba ",
-        nombreCapital: "Córdoba",
-        descripcion: "Es la Es una provincia ubicada en el centro del país, conocida por sus paisajes serranos y su historia colonial. Es un destino popular para el turismo interno, especialmente para aquellos que buscan aventuras al aire libre.",
-        imagenProvincia: "./assets/media/cordoba.jpg",
-        videoProvincia: "https://www.youtube.com/embed/FtD7hBacnTo",
+        nombreProvincia: "San Luis",
+        nombreCapital: "San Luis",
+        descripcion: "Esta provincia se encuentra en el noroeste de Argentina, y es conocida por su historia colonial y sus paisajes de montañas. Es el hogar de las ruinas de Quilmes, un importante sitio arqueológico que data de la época precolombina.",
+        imagenProvincia: "./assets/media/sanLuis.jpg",
+        videoProvincia: "https://www.youtube.com/watch?v=KYXsr3ADhoA",
         region:"Centro"
       }
     ]
@@ -107,9 +89,7 @@ export class ProvinciaService {
     localStorage.setItem("ListaProvincia",JSON.stringify(this.listaProvincias))
 
 
-  }
-
-  
+  }  
 
   getProvincias(): ProvinciaModel[] {
 
